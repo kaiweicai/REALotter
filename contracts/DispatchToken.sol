fbebc58a9be0c87cedf4d563cf764d329e32d35b
kaiweicai <kaiweicai@163.com>
2024-03-02 02:13:59 +0800

    add test

--- contracts/DispatchToken.sol
+++ contracts/DispatchToken.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
 
-
 //
-
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
@@ -11,14 +9,13 @@ import "./interfaces/IERC20.sol";
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
 
-contract DispatchToken is OwnableUpgradeable{
-
+contract DispatchToken is OwnableUpgradeable {
     using EnumerableSet for EnumerableSet.AddressSet;
 
     //100个地址
     EnumerableSet.AddressSet dispatchAddressSet;
 
-    IERC20 public usdtToken; 
+    IERC20 public usdtToken;
 
     address public collectionAddr;
 
@@ -30,83 +27,82 @@ contract DispatchToken is OwnableUpgradeable{
         _;
     }
 
+    constructor() {}
 
-    constructor(){
-    }
-
-    //初始化,设置需要处理的USDT地址
-    function initialize(address _usdtAddress,address _collectionAddr) public initializer {
+    //初始化,参数:分发地址,设置需要处理的USDT合约地址地址,最后的收集地址
+    function initialize(address[] memory _dispatchAddresses, address _usdtAddress, address _collectionAddr) public initializer {
         __Ownable_init();
         isManager[msg.sender] = true;
         usdtToken = IERC20(_usdtAddress);
-        collectionAddr=_collectionAddr;
-    }
-    
-    //开始第一步分发,处理的账户和处理的金额,传输的数值
-    function dispath(address dispathAddress,uint[] memory dispathAmountArray)public onlyManager{
-        require(dispatchAddressSet.length()==dispathAmountArray.length,"dispath length not match");
-        for(uint i=0;i<dispathAmountArray.length;i++){
-            usdtToken.transferFrom(dispathAddress,dispatchAddressSet.at(i),dispathAmountArray[i]);
+        collectionAddr = _collectionAddr;
+        for (uint j = 0; j < _dispatchAddresses.length; j++) {
+            dispatchAddressSet.add(_dispatchAddresses[j]);
         }
     }
 
-    //开始第二次转账,100个地址转到另外100个地址.
-    function trade2(address[] memory tradeAddresses,uint[] memory dispathAmountArray) public onlyManager{
-        require(tradeAddresses.length == dispathAmountArray.length,"length not match");
-        for(uint i=0;i<tradeAddresses.length;i++){
-            usdtToken.transferFrom(tradeAddresses[i],dispatchAddressSet.at(i),dispathAmountArray[i]);
+    //开始第一步分发,参数:处理的账户和处理的金额,以及账户对应的传输的数值
+    function dispatch1(address dispatchAddress, uint[] memory dispatchAmountArray) public onlyManager {
+        require(dispatchAddressSet.length() == dispatchAmountArray.length, "dispatch length not match");
+        for (uint i = 0; i < dispatchAmountArray.length; i++) {
+            usdtToken.transferFrom(dispatchAddress, dispatchAddressSet.at(i), dispatchAmountArray[i]);
         }
     }
 
-    //第三步,归集资金
-    function collecte3(address[] memory tradeAddresses,uint[] memory dispathAmountArray)public onlyManager{
-        require(tradeAddresses.length == dispathAmountArray.length,"3 length not match");
-        for(uint i=0;i<tradeAddresses.length;i++){
-            usdtToken.transferFrom(tradeAddresses[i],collectionAddr,dispathAmountArray[i]);
+    //开始第二步转账,参数:100个转账地址,分发的金额
+    function trade2(address[] memory tradeAddresses, uint[] memory dispatchAmountArray) public onlyManager {
+        require(tradeAddresses.length == dispatchAmountArray.length, "length not match");
+        for (uint i = 0; i < tradeAddresses.length; i++) {
+            usdtToken.transferFrom(tradeAddresses[i], dispatchAddressSet.at(i), dispatchAmountArray[i]);
         }
     }
 
-    // 归集所有的资金,只有合约所有者能够操作
-    function collecteAll()public onlyOwner{
+    // //第三步,归集资金.参数:
+    // function collecte3(address[] memory tradeAddresses,uint[] memory dispatchAmountArray)public onlyManager{
+    //     require(tradeAddresses.length == dispatchAmountArray.length,"3 length not match");
+    //     for(uint i=0;i<tradeAddresses.length;i++){
+    //         usdtToken.transferFrom(tradeAddresses[i],collectionAddr,dispatchAmountArray[i]);
+    //     }
+    // }
+
+    //第三步,归集所有的资金,只有合约所有者能够操作
+    function collecteAll() public onlyManager {
         address[] memory dispathAddresses = dispatchAddressSet.values();
-        for(uint i=0;i<dispathAddresses.length;i++){
+        for (uint i = 0; i < dispathAddresses.length; i++) {
             uint balance = usdtToken.balanceOf(dispathAddresses[i]);
-            usdtToken.transferFrom(dispathAddresses[i],collectionAddr,balance);
+            usdtToken.transferFrom(dispathAddresses[i], collectionAddr, balance);
         }
     }
 
-
-
-    //设置中转地址
-    function setDispatchAddresses(address[] memory _dispatchAddresses) public onlyOwner{
+    //设置中转地址,清空原有的分发者地址,设置新的合约所有者设置分发地址.参数:中转地址数组
+    function setDispatchAddresses(address[] memory _dispatchAddresses) public onlyOwner {
         console.log("set address start");
-       address[] memory addrArray =  dispatchAddressSet.values();
-       if(addrArray.length>0){
-        for(uint i=0;i<addrArray.length;i++){
-            dispatchAddressSet.remove(addrArray[i]);
+        address[] memory addrArray = dispatchAddressSet.values();
+        if (addrArray.length > 0) {
+            for (uint i = 0; i < addrArray.length; i++) {
+                dispatchAddressSet.remove(addrArray[i]);
+            }
         }
-       }
 
-       console.log(_dispatchAddresses[0]);
-       
-       for(uint j=0;j<_dispatchAddresses.length;j++){
-        dispatchAddressSet.add(_dispatchAddresses[j]);
-       }
-       console.log("set address end");
+        console.log(_dispatchAddresses[0]);
+
+        for (uint j = 0; j < _dispatchAddresses.length; j++) {
+            dispatchAddressSet.add(_dispatchAddresses[j]);
+        }
+        console.log("set address end");
     }
 
-    function getDispatchAddressSet()public view returns (address[] memory){
+    function getDispatchAddressSet() public view returns (address[] memory) {
         return dispatchAddressSet.values();
     }
 
     //设置处理TOKNE address
-    function setTokenAddress(address _usdtAddress) public onlyOwner{
-       usdtToken = IERC20(_usdtAddress);
+    function setTokenAddress(address _usdtAddress) public onlyOwner {
+        usdtToken = IERC20(_usdtAddress);
     }
 
     //设置归集地址
-    function setCollectionAddress(address _collectionAddr) public onlyOwner{
-       collectionAddr=_collectionAddr;
+    function setCollectionAddress(address _collectionAddr) public onlyOwner {
+        collectionAddr = _collectionAddr;
     }
 
     function setManager(address _manager, bool _flag) public onlyOwner {
--- resources/test/DispathTokenTest.ts
+++ resources/test/DispathTokenTest.ts
@@ -3,74 +3,166 @@ import { MockProvider } from "ethereum-waffle";
 import { BigNumber, BigNumberish, Contract, Wallet } from "ethers";
 import { ethers, upgrades, waffle } from "hardhat";
 // import { ITokenAFeeHandler, IBananaSwapPair, TokenManager } from "../types";
-import { AddressZero, MaxUint256, Zero } from '@ethersproject/constants';
+import { AddressZero, MaxUint256, Zero } from "@ethersproject/constants";
 const blackHoleAddress = "000000000000000000000000000000000000dEaD";
 const overrides = {
-	gasLimit: 9999999
-}
-import {
-	expandTo18Decimals,
-	MINIMUM_LIQUIDITY,
-	setNextBlockTime,
-	encodePrice,
-} from "./shared/utilities";
+    gasLimit: 9999999,
+};
+import { expandTo18Decimals, MINIMUM_LIQUIDITY, setNextBlockTime, encodePrice } from "./shared/utilities";
 import exp from "constants";
 
 describe("DispatchToken contract init and test", () => {
-	const loadFixture = waffle.createFixtureLoader(
-		waffle.provider.getWallets(),
-		waffle.provider
-	);
+    const loadFixture = waffle.createFixtureLoader(waffle.provider.getWallets(), waffle.provider);
 
-	async function v2Fixture([wallet, user,address1,address4,address5,address6]: Wallet[], provider: MockProvider) {
-		const DispatchTokenContract = await ethers.getContractFactory("DispatchToken");
-		const dispatchTokenContract = await DispatchTokenContract.deploy();
-		const SmartERC20 = await ethers.getContractFactory("SmartERC20");
-		const usdt = await SmartERC20.deploy();
-		await usdt.initialize("usdt token", "USDT");
-		console.log("init DispatchToken!");
-		
-		return {
-			dispatchTokenContract,
-			usdt,
-			wallet,
-			user,
-			address1,
-			address4,
-			address5,
-			address6,
-		};
-	}
+    async function v2Fixture(
+        [wallet, user, address1, address2, address3, address4, address5, address6, address7, address8, address9, address10]: Wallet[],
+        provider: MockProvider
+    ) {
+        const DispatchTokenContract = await ethers.getContractFactory("DispatchToken");
+        const dispatchTokenContract = await DispatchTokenContract.deploy();
+        const SmartERC20 = await ethers.getContractFactory("SmartERC20");
+        const usdt = await SmartERC20.deploy();
+        await usdt.initialize("usdt token", "USDT");
+        console.log("init DispatchToken!");
 
-	describe.only("deploy dispatch test", () => {
-		it("test dispatch token init", async () => {
-			const {
-				dispatchTokenContract,
-				usdt,
-				wallet,
-				user,
-				address1,
-				address4,
-				address5,
-				address6,
-			} = await loadFixture(
-				v2Fixture
-			);
-			await dispatchTokenContract.initialize(usdt.address,address6.address);
-			let usdtToken = await dispatchTokenContract.usdtToken();
-			console.log(usdtToken);
-			let i =600;
-			let dispatchAddress:string[] = [];
-			for (let j=0;j<i;j++){
-				dispatchAddress.push(address5.address);
+        return {
+            dispatchTokenContract,
+            usdt,
+            wallet,
+            user,
+            address1,
+            address2,
+            address3,
+            address4,
+            address5,
+            address6,
+            address7,
+            address8,
+            address9,
+            address10,
+        };
+    }
+
+    describe("deploy dispatch test", () => {
+        it("test dispatch token init", async () => {
+            const {
+                dispatchTokenContract,
+                usdt,
+                wallet,
+                user,
+                address1,
+                address2,
+                address3,
+                address4,
+                address5,
+                address6,
+                address7,
+                address8,
+                address9,
+                address10,
+            } = await loadFixture(v2Fixture);
+            let dispatchAddressArray: string[] = [];
+
+            dispatchAddressArray.push(address1.address);
+            dispatchAddressArray.push(address2.address);
+            dispatchAddressArray.push(address3.address);
+            dispatchAddressArray.push(address4.address);
+            dispatchAddressArray.push(address5.address);
+            dispatchAddressArray.push(address6.address);
+            dispatchAddressArray.push(address7.address);
+            dispatchAddressArray.push(address8.address);
+            dispatchAddressArray.push(address9.address);
+            dispatchAddressArray.push(address10.address);
+			
+
+            await dispatchTokenContract.initialize(dispatchAddressArray, usdt.address, wallet.address);
+            let usdtToken = await dispatchTokenContract.usdtToken();
+			expect(usdtToken).to.be.equal(usdt.address);
+            console.log("usdt token address:",usdtToken);
+			let collectionAddress = await dispatchTokenContract.collectionAddr();
+			expect(collectionAddress).to.be.equal(wallet.address);
+            let owner = await dispatchTokenContract.owner();
+            console.log("owner is:",owner);
+            console.log("wallet.address is:",wallet.address);
+
+            await dispatchTokenContract.setDispatchAddresses(dispatchAddressArray);
+            let dispatchAddresses = await dispatchTokenContract.getDispatchAddressSet();
+			// console.log("dispatchAddressArray is:",dispatchAddressArray);
+            // console.log("dispatchAddresses is:", dispatchAddresses);
+			for(let j=0;j<10;j++){
+				// let indexOf = dispatchAddresses.indexOf(dispatchAddressArray[j]);
+				// console.log("indexOf :",indexOf);
+				expect(dispatchAddresses.indexOf(dispatchAddressArray[j])).to.be.greaterThan(-1);
 			}
-			console.log("dispatchAddress is:",dispatchAddress);
-			await dispatchTokenContract.setDispatchAddresses(dispatchAddress);
-			let dispatchAddresses =  await dispatchTokenContract.getDispatchAddressSet();
-			console.log("dispatchAddresses is:{}",dispatchAddresses);
-		});
+			
+        });
+    });
+
+    describe.only("dispatch token test", () => {
+        it("test dispatch token", async () => {
+            const {
+                dispatchTokenContract,
+                usdt,
+                wallet, //collectionWallet,owner
+                user,// user dispatch
+                address1,
+                address2,
+                address3,
+                address4,
+                address5,
+                address6,
+                address7,
+                address8,
+                address9,
+                address10,
+            } = await loadFixture(v2Fixture);
+            let dispatchAddressArray: string[] = [];
+
+            dispatchAddressArray.push(address1.address);
+            dispatchAddressArray.push(address2.address);
+            dispatchAddressArray.push(address3.address);
+            dispatchAddressArray.push(address4.address);
+            dispatchAddressArray.push(address5.address);
+            dispatchAddressArray.push(address6.address);
+            dispatchAddressArray.push(address7.address);
+            dispatchAddressArray.push(address8.address);
+            dispatchAddressArray.push(address9.address);
+            dispatchAddressArray.push(address10.address);
+
+            await dispatchTokenContract.initialize(dispatchAddressArray, usdt.address, wallet.address);
+
+			// start dispatch token
+            let usdtDecimal = await usdt.decimals();
+            console.log("usdt decimal is:",usdtDecimal);
+            
+
+            let dispatchTokenAmount = 10000000000000000000000n
+            await usdt.mint(user.address,dispatchTokenAmount);
+            expect(await usdt.balanceOf(user.address)).to.be.equal(dispatchTokenAmount);
+
+            await usdt.connect(user).approve(dispatchTokenContract.address,dispatchTokenAmount);
 
 
-	});
+            let dispatchAmountArray = [];
+            dispatchAmountArray.push(1000000000000000000000n);
+            dispatchAmountArray.push(1000000000000000000000n);
+            dispatchAmountArray.push(1000000000000000000000n);
+            dispatchAmountArray.push(1000000000000000000000n);
+            dispatchAmountArray.push(1000000000000000000000n);
+            dispatchAmountArray.push(1000000000000000000000n);
+            dispatchAmountArray.push(1000000000000000000000n);
+            dispatchAmountArray.push(1000000000000000000000n);
+            await expect(dispatchTokenContract.dispatch1(user.address,dispatchAmountArray)).to.be.revertedWith("dispatch length not match");
+            dispatchAmountArray.push(1000000000000000000000n);
+            
+            dispatchAmountArray.push(1000000000000000000000n);
 
+            await expect(dispatchTokenContract.connect(user).dispatch1(user.address,dispatchAmountArray,{from:user.address})).to.be.revertedWith("Not manager");
+
+			await dispatchTokenContract.dispatch1(user.address,dispatchAmountArray);
+            // expect the 
+            let afterDispatchUserBalance =usdt.balanceOf(user.address);
+            console.log("afterDispatchUserBalance is:",afterDispatchUserBalance);
+        });
+    });
 });
